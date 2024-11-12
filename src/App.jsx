2d795c71c73e305868e1a87fd9359c257655d9ad
Dinesh Budhathoki <physmarika@gmail.com>
2024-11-12 11:43:54 +0800

    Basic Front End setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ToastContainer, toast } from "react-toastify";
+
+function App() {
+  toast.success("show your message");
+
+  return (
+    <>
+      Hellow world
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
